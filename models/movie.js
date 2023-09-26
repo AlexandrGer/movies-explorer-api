871f66116021d9766452a9a4f6fa2417d1d251e4
Alexandr Gerasimov <sashagerasimov55@gmail.com>
2023-09-26 11:18:13 +0300

    Последние изменения

--- models/movie.js
+++ models/movie.js
@@ -2,77 +2,76 @@ const mongoose = require('mongoose');
 const isUrl = require('validator/lib/isURL');
 
 const movieSchema = new mongoose.Schema({
-	country: {
-		type: String,
-		required: true,
-		minlength: 2,
-		maxlength: 30,
-	},
-	director: {
-		type: String,
-		required: true,
-		minlength: 2,
-		maxlength: 30,
-	},
-	duration: {
-		type: Number,
-		required: true,
-	},
-	year: {
-		type: String,
-		required: true,
-	},
-	description: {
-		type: String,
-		required: true,
-		minlength: 2,
-	},
-	image: {
-		type: String,
-		required: true,
-		validate: {
-			validator: (link) => isUrl(link),
-			message: 'Неверно указан URL',
-		},
-	},
-	trailerLink: {
-		type: String,
-		required: true,
-		validate: {
-			validator: (link) => isUrl(link),
-			message: 'Неверно указан URL',
-		},
-	},
-	thumbnail: {
-		type: String,
-		required: true,
-		validate: {
-			validator: (link) => isUrl(link),
-			message: 'Неверно указан URL',
-		},
-	},
-	owner: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'user',
-		required: true,
-	},
-	movieId: {
-		// type: mongoose.Schema.Types.Number,
-		type: Number,
-		required: true,
-	},
-	nameRU: {
-		type: String,
-		required: true,
-		minlength: 2,
-		maxlength: 30,
-	},
-	nameEN: {
-		type: String,
-		required: true,
-		minlength: 2,
-		maxlength: 30,
-	},
+  country: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  director: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    minlength: 2,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (link) => isUrl(link),
+      message: 'Неверно указан URL',
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (link) => isUrl(link),
+      message: 'Неверно указан URL',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (link) => isUrl(link),
+      message: 'Неверно указан URL',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: Number,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
 });
 
 module.exports = mongoose.model('movie', movieSchema);
