52616ba8d2bce9afedabb4f39fa249fc59987c5b
Alexandr Gerasimov <sashagerasimov55@gmail.com>
2023-09-27 09:47:17 +0300

    Исправил замечания ревью v2

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ const handleError = require('./errors/handleError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const limiter = require('./middlewares/limiter');
 
-const { PORT, DB_URL } = process.env;
+const {
+  PORT = 3000,
+  DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb',
+} = process.env;
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
